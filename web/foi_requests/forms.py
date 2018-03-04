a62e8ce96a4be3356e04797c190f2f463a05c9f1
Leo Tartari <ltartari@gmail.com>
2018-03-04 17:08:50 -0300

    Add basic styling w/ Tachyons

--- web/foi_requests/forms.py
+++ web/foi_requests/forms.py
@@ -1,8 +1,8 @@
-from django import forms
+from django.forms import ModelForm, Textarea, CharField
 from .models import Message
 
 
-class MessageForm(forms.ModelForm):
+class MessageForm(ModelForm):
     class Meta:
         model = Message
         fields = [
@@ -10,5 +10,13 @@ class MessageForm(forms.ModelForm):
             'title',
             'body',
         ]
+        # TODO: clean this up once we have widget_tweaks
+        classes = 'db border-box hover-black w-100'
+        classes += ' measure ba b--black-20 pa2 br2 mb2'
+        widgets = {
+            'body': Textarea(attrs={'class': classes}),
+        }
 
-    title = forms.CharField()
+    # TODO https://github.com/jazzband/django-widget-tweaks
+    # install above dependency to ease the class adding process
+    title = CharField()
