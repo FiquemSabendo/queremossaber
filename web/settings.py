1d90f263015bc3c0a92704620aa1c9ed37eb4b7e
Moises Rodrigues <moisesr@spotify.com>
2018-03-24 19:45:18 +0100

    Upload de arquivos a uma resposta

--- web/settings.py
+++ web/settings.py
@@ -14,7 +14,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -118,3 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Uploaded files variables
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
--- web/urls.py
+++ web/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from django.views.generic import TemplateView
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('p/', include('web.foi_requests.urls')),
     path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT, 'show_indexes': False}),
 ]
+
+
