64e33093c403dee0a28c98bafafb381a6726dd2c
Leo Tartari <ltartari@gmail.com>
2018-03-28 22:40:17 -0300

    Use widget-tweaks for classes on form inputs

--- web/foi_requests/forms.py
+++ web/foi_requests/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, Textarea, CharField
+from django.forms import ModelForm, CharField
 from .models import Message
 
 
@@ -10,13 +10,5 @@ class MessageForm(ModelForm):
             'title',
             'body',
         ]
-        # TODO: clean this up once we have widget_tweaks
-        classes = 'db border-box hover-black w-100'
-        classes += ' measure ba b--black-20 pa2 br2 mb2'
-        widgets = {
-            'body': Textarea(attrs={'class': classes}),
-        }
 
-    # TODO https://github.com/jazzband/django-widget-tweaks
-    # install above dependency to ease the class adding process
     title = CharField()
