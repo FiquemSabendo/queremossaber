38d75d8dd0ba9f36ae1266abe14d8508d02d20ce
Vitor Baptista <vitor@vitorbaptista.com>
2018-07-17 11:57:46 +0100

    Set timezone and language to Brazilian Portuguese

--- web/settings.py
+++ web/settings.py
@@ -111,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Brazil/East'
 
 USE_I18N = True
 
