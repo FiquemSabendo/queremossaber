b70adc6c9ba24bc3b059d103f03f8542f06ff9ac
Vitor Baptista <vitor@vitorbaptista.com>
2018-07-17 15:34:35 +0100

    Allow user to create new PublicBody if it doesn't exist

--- web/foi_requests/urls.py
+++ web/foi_requests/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.FOIRequestRedirectView.as_view(), name='foirequest_search'),
     path('new/', views.CreateMessageView.as_view(), name='message_new'),
+    path('public_body/new/', views.CreatePublicBodyView.as_view(), name='publicbody_new'),
     re_path(
         '^(?P<slug>[\w\d]+)/$',
         views.FOIRequestView.as_view(),
--- web/foi_requests/views.py
+++ web/foi_requests/views.py
@@ -4,12 +4,31 @@ from django.views.generic.base import RedirectView
 from django.urls import reverse
 
 from .forms import MessageForm
-from .models import FOIRequest
+from .models import FOIRequest, PublicBody
 
 
 class CreateMessageView(CreateView):
     form_class = MessageForm
     template_name = 'foi_requests/message_new.html'
+
+    def get_initial(self):
+        return {
+            'receiver': self.request.GET.get('receiver'),
+        }
+
+
+class CreatePublicBodyView(CreateView):
+    model = PublicBody
+    fields = [
+        'name',
+        'esic_url',
+    ]
+
+    def get_success_url(self):
+        return '{url}?receiver={receiver}'.format(
+            url=reverse('message_new'),
+            receiver=self.object.id
+        )
 
 
 class FOIRequestView(DetailView):
