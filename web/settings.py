124961a92875c583c8c85e2d1bd4d406bedcf194
Victória César <victoriacesaras@gmail.com>
2024-09-13 10:20:26 -0300

    fix: remove default from env

--- web/settings.py
+++ web/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ENV = env("ENV", default="dev")
+ENV = env("ENV")
 
 ALLOWED_HOSTS = (
     [
